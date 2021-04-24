f5c4079db6f110a99ab2998dcc473eaa0d596007
DmitryBahtenkov <d.bahtenkov@skillaz.ru>
2021-04-24 14:37:34 +0300

    folders

---
+++ FlexTesting.Core.Contract/Folder/Dtos/CreateFolderDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlexTesting.Core.Contract.Folder.Dtos
+{
+    public record CreateFolderDto
+    {
+        [Required(ErrorMessage = "Введите название директории")]
+        public string Name { get; set; }
+        [Required]
+        public string UserId { get; set; }
+    }
+}
---
+++ FlexTesting.Core.Contract/Folder/IFolderService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FlexTesting.Core.Contract.Folder.Dtos;
+
+namespace FlexTesting.Core.Contract.Folder
+{
+    public interface IFolderService
+    {
+        public Task<Models.Folder> CreateFolder(CreateFolderDto createFolderDto);
+        public Task<Models.Folder> DeleteFolder(string id);
+        public Task<IEnumerable<Models.Folder>> GetByUser(string userId);
+    }
+}
