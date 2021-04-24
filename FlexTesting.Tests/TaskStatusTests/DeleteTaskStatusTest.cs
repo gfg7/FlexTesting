06adcc6ad7289887505c21a8e035da54fb5a5c63
DmitryBahtenkov <d.bahtenkov@skillaz.ru>
2021-04-24 16:59:02 +0300

    delete taskstatus tests

---
+++ FlexTesting.Tests/TaskStatusTests/DeleteTaskStatusTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using FlexTesting.Core.Contract.Exceptions;
+using FlexTesting.Tests.Helpers;
+using NUnit.Framework;
+
+namespace FlexTesting.Tests.TaskStatusTests
+{
+    [TestFixture]
+    public class DeleteTaskStatusTest : BaseTaskStatusTest
+    {
+        public async Task DeleteExistingStatus()
+        {
+            var result = await _taskStatusService.Delete(StatusHelper.DeletionStatus.Id);
+            Assert.True(result.IsDeleted);
+        }
+
+        public async Task DeleteNotExistingStatus()
+        {
+            Assert.ThrowsAsync<NotFoundException>(async () => await _taskStatusService.Delete("aa"));
+        }
+    }
+}
