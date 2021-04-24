f9ba6efe193d35766342d874267a74ed99d71511
DmitryBahtenkov <d.bahtenkov@skillaz.ru>
2021-04-24 15:19:39 +0300

    rename folder tests

---
+++ FlexTesting.Tests/FolderTests/RenameFolderTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using FlexTesting.Core.Contract.Exceptions;
+using FlexTesting.Core.Contract.Folder.Dtos;
+using FlexTesting.Tests.Helpers;
+using FlexTesting.Tests.UserTests;
+using NUnit.Framework;
+
+namespace FlexTesting.Tests.FolderTests
+{
+    [TestFixture]
+    public class RenameFolderTest : BaseFolderTest
+    {
+        [Test]
+        public async Task RenameExistingFolder()
+        {
+            var dto = new RenameFolderDto
+            {
+                FolderId = FolderHelper.ValidFolder.Id,
+                NewName = "New Name"
+            };
+
+            var result = await _folderService.RenameFolder(dto);
+            Assert.AreEqual(result.Name, dto.NewName);
+        }
+
+        [Test]
+        public async Task RenameNotExistingFolder()
+        {
+            var dto = new RenameFolderDto
+            {
+                NewName = "a",
+                FolderId = "aaaaa"
+            };
+
+            Assert.ThrowsAsync<NotFoundException>(async () => await _folderService.RenameFolder(dto));
+        }
+    }
+}
