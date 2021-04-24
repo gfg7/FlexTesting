e280ee99e5797c36c4db92aa578a256ec43bf6ef
DmitryBahtenkov <d.bahtenkov@skillaz.ru>
2021-04-24 15:11:12 +0300

    get folder tests

---
+++ FlexTesting.Tests/FolderTests/GetFolderByUserTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using FlexTesting.Core.Contract.Exceptions;
+using FlexTesting.Tests.Helpers;
+using NUnit.Framework;
+
+namespace FlexTesting.Tests.FolderTests
+{
+    [TestFixture]
+    public class GetFolderByUserTest : BaseFolderTest 
+    {
+        [Test]
+        public async Task GetExistingFolders()
+        {
+            var result = await _folderService.GetByUser(UserHelper.UserModel.Id);
+            Assert.IsNotEmpty(result);
+        }
+
+        [Test]
+        public async Task GetNotExistingFolders()
+        {
+            Assert.ThrowsAsync<NotFoundException>(async () => await _folderService.GetByUser("a"));
+        }
+    }
+}
