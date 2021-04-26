7e7e32c90bf69736f0645162979a4da0c56cd224
Dmitry Bahtenkov <cool.iridiy12soul@gmail.com>
2021-04-26 21:37:47 +0300

    fix template

--- FlexTesting.Core.Contract/Operations/GetOperations/IGetOperations.cs
+++ FlexTesting.Core.Contract/Operations/GetOperations/IGetOperations.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using FlexTesting.Core.Contract.Models;
 
 namespace FlexTesting.Core.Contract.Operations.GetOperations
 {
-    public interface IGetOperations<TModel>
+    public interface IGetOperations<TModel> where TModel : IModel
     {
         public Task<TModel> GetById(string id);
         public Task<IEnumerable<TModel>> GetAll(string id);
