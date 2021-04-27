595e0cb863dcb2bf640a23058140fc466b3caae6
Dmitry Bahtenkov <cool.iridiy12soul@gmail.com>
2021-04-27 21:43:44 +0300

    initial web-app

---
+++ FlexTesting.WebApp/Startup.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlexTesting.Core.Contract.Folder;
+using FlexTesting.Core.Contract.Issue;
+using FlexTesting.Core.Contract.TaskStatus;
+using FlexTesting.Core.Contract.Token;
+using FlexTesting.Core.Contract.User;
+using FlexTesting.Core.Folder;
+using FlexTesting.Core.Issue;
+using FlexTesting.Core.TaskStatus;
+using FlexTesting.Core.Token;
+using FlexTesting.Core.User;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FlexTesting.WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IUserGetOperations, UserGetOperations>();
+            services.AddScoped<IUserWriteOperations, UserWriteOperations>();
+
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<ITokenGetOperations, TokenGetOperations>();
+            services.AddScoped<ITokenWriteOperations, TokenWriteOperations>();
+            
+            services.AddScoped<IFolderService, FolderService>();
+            services.AddScoped<IFolderGetOperations, FolderGetOperations>();
+            services.AddScoped<IFolderWriteOperations, FolderWriteOperations>();
+            
+            services.AddScoped<ITaskStatusService, TaskStatusService>();
+            services.AddScoped<ITaskStatusGetOperations, StatusGetOperations>();
+            services.AddScoped<ITaskStatusWriteOperations, StatusWriteOperations>();
+            
+            services.AddScoped<IIssueService, IssueService>();
+            services.AddScoped<IIssueGetOperations, IssueGetOperations>();
+            services.AddScoped<IIssueWriteOperations, IssueWriteOperations>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
