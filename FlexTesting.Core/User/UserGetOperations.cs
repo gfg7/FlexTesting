a8095d5a4ca1d2cc96eecf4bdc746955b01ef989
DmitryBahtenkov <dmitrybahtenkov@gmail.com>
2021-06-20 19:01:11 +0300

    user

--- FlexTesting.Core/User/UserGetOperations.cs
+++ FlexTesting.Core/User/UserGetOperations.cs
@@ -7,40 +7,20 @@ using MongoDB.Driver;
 
 namespace FlexTesting.Core.User
 {
-    public class UserGetOperations : IUserGetOperations
+    public class UserGetOperations : GetOperations<Contract.Models.User>, IUserGetOperations
     {
-        private readonly IUserContext _userContext;
-        public UserGetOperations()
-        {
-            _userContext = DbContext.UserContext;
-        }
-        public async Task<Contract.Models.User> GetById(string id)
-        {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Id, id);
-            return await _userContext.Users.Find(filter).FirstOrDefaultAsync();
-        }
-
-        public async Task<IEnumerable<Contract.Models.User>> GetAll()
-        {
-            return await _userContext.Users.Find(new BsonDocument()).ToListAsync();
-        }
-
-        public async Task<bool> ExistsById(string id)
-        {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Id, id);
-            return await _userContext.Users.CountDocumentsAsync(filter) != 0;
-        }
-
         public async Task<Contract.Models.User> ByUserName(string userName)
         {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Login, userName);
-            return await _userContext.Users.Find(filter).FirstOrDefaultAsync();
+            return await GetOne(F.Eq(x => x.Login, userName));
         }
 
         public async Task<Contract.Models.User> ByToken(string token)
         {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Token, token);
-            return await _userContext.Users.Find(filter).FirstOrDefaultAsync();
+            return await GetOne(F.Eq(x => x.Login, token));
+        }
+
+        public UserGetOperations(DbContext dbContext) : base(dbContext)
+        {
         }
     }
 }
--- FlexTesting.Core/User/UserWriteOperations.cs
+++ FlexTesting.Core/User/UserWriteOperations.cs
@@ -6,100 +6,52 @@ using MongoDB.Driver;
 
 namespace FlexTesting.Core.User
 {
-    public class UserWriteOperations : IUserWriteOperations
+    public class UserWriteOperations : WriteOperations<Contract.Models.User>, IUserWriteOperations
     {
-        private readonly IUserContext _userContext;
-
-        public UserWriteOperations()
-        {
-            _userContext = DbContext.UserContext;
-        }
-        public async Task<Contract.Models.User> Create(Contract.Models.User item)
-        {
-            await _userContext.Users.InsertOneAsync(item);
-            return item;
-        }
-
-        public async Task<Contract.Models.User> Update(string id, Contract.Models.User item)
-        {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Id, id);
-            var result = await _userContext.Users.UpdateOneAsync(filter, new ObjectUpdateDefinition<Contract.Models.User>(item));
-            return result.IsAcknowledged ? item : null;
-        }
-
-        public async Task<Contract.Models.User> Delete(string id)
-        {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Id, id);
-            var item = await _userContext.Users.FindSync(filter).FirstOrDefaultAsync();
-            var result = await _userContext.Users.DeleteOneAsync(filter);
-            return result.IsAcknowledged ? item : null;
-        }
-
-        public async Task<Contract.Models.User> SafeDelete(string id)
-        {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Id, id);
-            var update = Builders<Contract.Models.User>.Update.Set(x => x.IsDeleted, true);
-            var result = await _userContext.Users.UpdateOneAsync(filter, update);
-            return result.IsAcknowledged ? await _userContext.Users.FindSync(filter).FirstOrDefaultAsync() : null;
-        }
-
         public async Task<Contract.Models.User> UpdatePassword(string userId, string password, string salt)
         {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Id, userId);
-            var update = Builders<Contract.Models.User>.Update.Set(x => x.Password, password).Set(x=>x.Salt, salt);
-            var result = await _userContext.Users.UpdateOneAsync(filter, update);
-            return result.IsAcknowledged ? await _userContext.Users.FindSync(filter).FirstOrDefaultAsync() : null;
+            return await UpdateOne(
+                F.Eq(x => x.Id, userId), 
+                U.Set(x => x.Password, password).Set(x => x.Salt, salt));
         }
 
         public async Task<Contract.Models.User> UpdateUsername(string userId, string userName)
         {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Id, userId);
-            var update = Builders<Contract.Models.User>.Update.Set(x => x.Login, userName);
-            var result = await _userContext.Users.UpdateOneAsync(filter, update);
-            return result.IsAcknowledged ? await _userContext.Users.FindSync(filter).FirstOrDefaultAsync() : null;
+            return await UpdateOne(F.Eq(x => x.Id, userId), U.Set(x => x.Login, userName));
         }
 
         public async Task<Contract.Models.User> UpdateFio(string userId, string firstName, string lastName, string middleName)
         {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Id, userId);
-            var update = Builders<Contract.Models.User>.Update
+            var filter = F.Eq(x => x.Id, userId);
+            var update = U
                 .Set(x => x.FirstName, firstName)
                 .Set(x=>x.LastName, lastName)
                 .Set(x=>x.MiddleName, middleName);
-            var result = await _userContext.Users.UpdateOneAsync(filter, update);
-            return result.IsAcknowledged ? await _userContext.Users.FindSync(filter).FirstOrDefaultAsync() : null;
+            return await UpdateOne(filter, update);
         }
 
         public async Task<Contract.Models.User> SetToken(string userId, string token)
         {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Id, userId);
-            var update = Builders<Contract.Models.User>.Update.Set(x => x.Token, token);
-            var result = await _userContext.Users.UpdateOneAsync(filter, update);
-            return result.IsAcknowledged ? await _userContext.Users.FindSync(filter).FirstOrDefaultAsync() : null;
+            return await UpdateOne(F.Eq(x => x.Id, userId), U.Set(x => x.Token, token));
         }
 
         public async Task<Contract.Models.User> UnsetToken(string token)
         {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Token, token);
-            var update = Builders<Contract.Models.User>.Update.Set(x => x.Token, null);
-            var result = await _userContext.Users.UpdateOneAsync(filter, update);
-            return result.IsAcknowledged ? await _userContext.Users.FindSync(filter).FirstOrDefaultAsync() : null;
+            return await UpdateOne(F.Eq(x => x.Token, token), U.Set(x => x.Token, null));
         }
 
         public async Task<Contract.Models.User> ConfirmEmail(string userId)
         {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Id, userId);
-            var update = Builders<Contract.Models.User>.Update.Set(x => x.IsEmailConfirmed, true);
-            var result = await _userContext.Users.UpdateOneAsync(filter, update);
-            return result.IsAcknowledged ? await _userContext.Users.FindSync(filter).FirstOrDefaultAsync() : null;
+            return await UpdateOne(F.Eq(x => x.Id, userId), U.Set(x => x.IsEmailConfirmed, true));
         }
 
         public async Task<Contract.Models.User> SetEmailCode(string userId, string code)
         {
-            var filter = Builders<Contract.Models.User>.Filter.Eq(x => x.Id, userId);
-            var update = Builders<Contract.Models.User>.Update.Set(x => x.EmailCode, code);
-            var result = await _userContext.Users.UpdateOneAsync(filter, update);
-            return result.IsAcknowledged ? await _userContext.Users.FindSync(filter).FirstOrDefaultAsync() : null;
+            return await UpdateOne(F.Eq(x => x.Id, userId), U.Set(x => x.EmailCode, code));
+        }
+
+        public UserWriteOperations(DbContext dbContext) : base(dbContext)
+        {
         }
     }
 }
