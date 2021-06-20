172d42280dbe94a761c3254095b833231bc8c64e
DmitryBahtenkov <dmitrybahtenkov@gmail.com>
2021-06-20 18:48:20 +0300

    issue operations

--- FlexTesting.Core/Issue/IssueWriteOperations.cs
+++ FlexTesting.Core/Issue/IssueWriteOperations.cs
@@ -7,8 +7,6 @@ namespace FlexTesting.Core.Issue
 {
     public class IssueWriteOperations : WriteOperations<Contract.Models.Issue>, IIssueWriteOperations
     {
-        private readonly IIssueContext _issueContext;
-        
         public async Task<Contract.Models.Issue> UpdateName(string issueId, string name)
         {
             return await UpdateOne(F.Eq(x => x.Id, issueId), U.Set(x => x.Name, name));
@@ -16,31 +14,22 @@ namespace FlexTesting.Core.Issue
 
         public async Task<Contract.Models.Issue> UpdateDescription(string issueId, string description)
         {
-            var filter = Builders<Contract.Models.Issue>.Filter.Eq(x => x.Id, issueId);
-            var update = Builders<Contract.Models.Issue>.Update.Set(x => x.Description, description);
-            var result = await _issueContext.Issues.UpdateOneAsync(filter, update);
-            return result.IsAcknowledged ? await _issueContext.Issues.FindSync(filter).FirstOrDefaultAsync() : null;
+            return await UpdateOne(F.Eq(x => x.Id, issueId), U.Set(x => x.Description, description));
         }
 
         public async Task<Contract.Models.Issue> UpdateFolder(string issueId, string folderId)
         {
-            var filter = Builders<Contract.Models.Issue>.Filter.Eq(x => x.Id, issueId);
-            var update = Builders<Contract.Models.Issue>.Update.Set(x => x.FolderId, folderId);
-            var result = await _issueContext.Issues.UpdateOneAsync(filter, update);
-            return result.IsAcknowledged ? await _issueContext.Issues.FindSync(filter).FirstOrDefaultAsync() : null;
+            return await UpdateOne(F.Eq(x => x.Id, issueId), U.Set(x => x.FolderId, folderId));
         }
 
         public async Task DeleteByFolder(string folderId)
         {
-            await _issueContext.Issues.DeleteManyAsync(x => x.FolderId == folderId);
+            await Collection.DeleteManyAsync(x => x.FolderId == folderId);
         }
 
         public async Task<Contract.Models.Issue> UpdateStatus(string issueId, string statusId)
         {
-            var filter = Builders<Contract.Models.Issue>.Filter.Eq(x => x.Id, issueId);
-            var update = Builders<Contract.Models.Issue>.Update.Set(x => x.StatusId, statusId);
-            var result = await _issueContext.Issues.UpdateOneAsync(filter, update);
-            return result.IsAcknowledged ? await _issueContext.Issues.FindSync(filter).FirstOrDefaultAsync() : null;
+            return await UpdateOne(F.Eq(x => x.Id, issueId), U.Set(x => x.StatusId, statusId));
         }
 
         public IssueWriteOperations(DbContext dbContext) : base(dbContext)
